f6f486008707feda878488c247486225719bb794
Stig B. Dørmænen <stigbd@gmail.com>
2018-06-29 13:43:05 +0200

    Rename parameter and add DELETE in spec and test

--- app/test/pets-{id}-test.js
+++ app/test/pets-{id}-test.js
@@ -4,9 +4,9 @@ var chakram = require('chakram');
 var request = chakram.request;
 var expect = chakram.expect;
 
-describe('tests for /pets/{petId}', function() {
+describe('tests for /pets/{id}', function() {
     describe('tests for get', function() {
-        it('should respond 200 for "A pets with the given id."', function() {
+        it('should respond 200 for "A pet with the given id."', function() {
             var response = request('get', 'http://localhost:8080/pets/1', { 
                 'headers': {"Content-Type":"application/json","Accept":"application/json"},
                 'time': true
@@ -14,6 +14,18 @@ describe('tests for /pets/{petId}', function() {
 
             expect(response).to.have.status(200)
             expect(response).to.have.schema({"type":"object","items":{"type":"object","required":["id","name"],"properties":{"id":{"type":"integer","format":"int64"},"name":{"type":"string"},"tag":{"type":"string"}}}});
+            return chakram.wait();
+        });    
+    });
+    
+    describe('tests for delete', function() {
+        it('should respond 204 for "pet deleted"', function() {
+            var response = request('delete', 'http://localhost:8080/pets/1', { 
+                'headers': {"Content-Type":"application/json","Accept":"application/json"},
+                'time': true
+            });
+
+            expect(response).to.have.status(204)
             return chakram.wait();
         });    
     });
