91898fceb014e1fdde9904de9b228a80fb11ac9f
Stig B. Dørmænen <stigbd@gmail.com>
2018-08-01 18:40:17 +0200

    Fix response-time metrics

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const responseTime = require('response-time');
 
 var StatsD = require('hot-shots');
 const options = {
-  "host": "telegraf",
+  "host": "localhost",
   "port": "8125"
 }
 var metrics = new StatsD(options);
@@ -17,12 +17,6 @@ metrics.socket.on('error', function(error) {
   console.error("Error in socket: ", error);
 });
 
-app.use(responseTime(function (req, res, time) {
-  var stat = (req.method + req.url).toLowerCase()
-    .replace(/[:\.]/g, '')
-    .replace(/\//g, '_')
-  pets.timing(stat, time)
-}))
 app.use(logger());
 app.use(bodyParser());
 
@@ -35,29 +29,36 @@ var maxId = 3;
 
 const pets = {
   list: (ctx) => {
+    var starttimer = Date.now();
     ctx.body = db;
+    metrics.timing('pets.timer_list', Date.now() - starttimer);
     metrics.increment('pets.counter_list');
   },
 
   create: (ctx) => {
+    var starttimer = Date.now();
     console.log('Creating: ', ctx.request.body);
     var index = db.push(ctx.request.body);
     db[index-1].id = ++maxId;
     ctx.set('Location', 'http://localhost:8080/pets/' + ctx.request.body.id);
     ctx.status = 201;
+    metrics.timing('pets.timer_list', Date.now() - starttimer);
     metrics.increment('pets.counter_create');
   },
 
   show: (ctx, id) => {
-    var pet = db.find( o => o.id === parseInt(id));
+    var starttimer = Date.now();
     if (!pet) return ctx.throw(404, 'cannot find that pet');
+    var pet = db.find( o => o.id === parseInt(id));
     ctx.body = pet;
+    metrics.timing('pets.timer_show', Date.now() - starttimer);
     metrics.increment('pets.counter_show');
   },
 
   update: (ctx, id) => {
-    console.log('Updating: ', id, ' ', ctx.request.body);
+    var starttimer = Date.now();
     var pet = db.find( o => o.id === parseInt(id));
+    console.log('Updating: ', id, ' ', ctx.request.body);
     if (!pet) return ctx.throw(404, 'cannot find that pet');
     var index = db.indexOf(pet);
     if (index > -1) {
@@ -65,10 +66,12 @@ const pets = {
       db[index].id = pet.id;
     }
     ctx.status = 204;
+    metrics.timing('pets.timer_update', Date.now() - starttimer);
     metrics.increment('pets.counter_update');
 },
 
   delete: (ctx, id) => {
+    var starttimer = Date.now();
     console.log('Deleting: ', id);
     var pet = db.find( o => o.id === parseInt(id));
     if (!pet) return ctx.throw(404, 'cannot find that pet');
@@ -77,15 +80,16 @@ const pets = {
       db.splice(index, 1);
     }
     ctx.status = 204;
+    metrics.timing('pets.timer_delete', Date.now() - starttimer);
     metrics.increment('pets.counter_delete');
   }
 };
 
 app.use(_.get('/pets', pets.list));
 app.use(_.post('/pets', pets.create));
-app.use(_.get('/pets/:id', pets.show))
-app.use(_.put('/pets/:id', pets.update))
-app.use(_.delete('/pets/:id', pets.delete))
+app.use(_.get('/pets/:id', pets.show));
+app.use(_.put('/pets/:id', pets.update));
+app.use(_.delete('/pets/:id', pets.delete));
 
 const server = app.listen(8080, function (){
   console.log('listening on port 8080');
