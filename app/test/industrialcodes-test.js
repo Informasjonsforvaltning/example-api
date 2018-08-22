110ea54e6f5ac08ee7bd3d807903bdf312510bab
Stig B. Dørmænen <stigbd@gmail.com>
2018-08-22 10:57:13 +0200

    Refactor from pets to industrial codes #4

--- app/test/industrialcodes-test.js
+++ app/test/industrialcodes-test.js
@@ -4,24 +4,24 @@ var chakram = require('chakram');
 var request = chakram.request;
 var expect = chakram.expect;
 
-describe('tests for /pets', function() {
+describe('tests for /industrialcodes', function() {
     describe('tests for get', function() {
-        it('should respond 200 for "A list of pets."', function() {
-            var response = request('get', 'http://localhost:8080/pets', { 
+        it('should respond 200 for "A list of industrial codes."', function() {
+            var response = request('get', 'http://localhost:8080/industrialcodes', { 
                 'headers': {"Content-Type":"application/json","Accept":"application/json"},
                 'time': true
             });
 
             expect(response).to.have.status(200)
-            expect(response).to.have.schema({"type":"array","items":{"type":"object","required":["id","name"],"properties":{"id":{"type":"integer","format":"int64"},"name":{"type":"string"},"species":{"type":"string"}}}});
+            expect(response).to.have.schema({"type":"array","items":{"$ref":"#/definitions/industrialcode"}});
             return chakram.wait();
         });    
     });
     
     describe('tests for post', function() {
         it('should respond 201 for "Created"', function() {
-            var response = request('post', 'http://localhost:8080/pets', { 
-                'body': {"name":"eiusmod cupidatat minim","species":"incididunt"},
+            var response = request('post', 'http://localhost:8080/industrialcodes', { 
+                'body': {"name":"id occaecat nulla","species":"qui ut"},
                 'headers': {"Content-Type":"application/json","Accept":"application/json"},
                 'time': true
             });
--- features/pets.feature
+++ features/pets.feature
@@ -1,8 +1,8 @@
-Feature: Pet list
-  In order to get information about pets
+Feature: Industrial code list
+  In order to get information about industrial codes
   As a service-consumer (client)
-  I want to get a list of pets
+  I want to get a list of industrial codes
 
-  Scenario: list pets
-  When the client requests GET /pets
+  Scenario: List industrial codes
+  When the client requests GET /industrialcode
   Then the response should be JSON
