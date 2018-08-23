3755c687ff089dd52e657bff3ed5fbd36edd59b7
Stig B. Dørmænen <stigbd@gmail.com>
2018-08-23 18:28:35 +0200

    Fix 2.0 spec

--- app/test/generateTest.js
+++ app/test/generateTest.js
@@ -2,11 +2,11 @@ var oatts = require('oatts');
 
 var options = {
     // see "Options" section below for available options
-    "spec": "./openAPI/openapi.yaml",
+    "spec": "./openAPI/industrialcodes_2.0.yaml",
     "customValuesFile": "./test/customValues.json",
     "writeTo": "./test"
 };
 
-var tests = oatts.generate('./openAPI/openapi.yaml', options);
+var tests = oatts.generate('./openAPI/industrialcodes_2.0.yaml', options);
 
 console.log(tests)
